878ae4acc056eaa82be25900a9edc0a4c6fb1a21
endymion1818 <endymion1818@gmail.com>
2022-03-21 10:46:25 +0000

    simple hydrated component

--- _includes/Counter.svelte
+++ _includes/Counter.svelte
@@ -1,17 +1,10 @@
-<script context="module">
-  export const frontMatter = {
-    hydrate: "eager",
-  };
-</script>
-
 <script>
-	let count = 0;
+  let count = 0;
 
-	function handleClick() {
-		count += 1;
-	}
+  function add() {
+    count += 1;
+  }
 </script>
 
-<button on:click={handleClick}>
-	Clicked {count} {count === 1 ? 'time' : 'times'}
-</button>
+<p>You've clicked the button {count} times</p>
+<button on:click={add}>Add one</button>
