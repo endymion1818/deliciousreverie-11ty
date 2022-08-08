af4ea04d984864acedf0bc1000578471276e685b
Ben Read <endymion1818@gmail.com>
2022-08-08 11:44:16 +0100

    fix(config): disable slinkity :cry:

--- .eleventy.js
+++ .eleventy.js
@@ -41,7 +41,6 @@ module.exports = function(eleventyConfig) {
     return array.slice(0, n);
   });
 
-  // Return the smallest number argument
   eleventyConfig.addFilter("min", (...numbers) => {
     return Math.min.apply(null, numbers);
   });
@@ -98,42 +97,23 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("_redirects");
 
-  eleventyConfig.addPlugin(slinkity.plugin, slinkity.defineConfig({
-    // optional: use slinkity.defineConfig
-    // for some handy autocomplete in your editor
-  }))
+  // Turning off until we can fix issues with xml sitemaps
+  // @see https://github.com/slinkity/slinkity/issues/65
+  // eleventyConfig.addPlugin(slinkity.plugin, slinkity.defineConfig({
+
+  // }))
 
   return {
-    // Control which files Eleventy will process
-    // e.g.: *.md, *.njk, *.html, *.liquid
     templateFormats: [
       "md",
       "njk",
       "html",
-      "liquid"
+      "liquid",
+      "xml"
     ],
-
-    // Pre-process *.md files with: (default: `liquid`)
     markdownTemplateEngine: "njk",
-
-    // Pre-process *.html files with: (default: `liquid`)
     htmlTemplateEngine: "njk",
-
-    // -----------------------------------------------------------------
-    // If your site deploys to a subdirectory, change `pathPrefix`.
-    // Don’t worry about leading and trailing slashes, we normalize these.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-
-    // Optional (default is shown)
     pathPrefix: "/",
-    // -----------------------------------------------------------------
-
-    // These are all optional (defaults are shown):
     dir: {
       input: ".",
       includes: "_includes",
